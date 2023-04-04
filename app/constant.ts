a2f6c7ebfbabd7042a31dbdbfc91c3224c4742de
xingke2023 <xiaomi@xingke888.com>
2023-04-04 10:45:57 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,6 +2,6 @@ export const OWNER = "人工智能写作";
 export const REPO = "ChatGPT";
 export const REPO_URL = `www.xingke888.com`;
 export const ISSUE_URL = `www.xingke888.com`;
-export const UPDATE_URL = `www.chatgpt4chrome.com`;
+export const UPDATE_URL = `www.chatgpt4china.com`;
 export const FETCH_COMMIT_URL = `www.chatgpt4china.com`;
 export const FETCH_TAG_URL = `www.chatgpt4china.com`;
